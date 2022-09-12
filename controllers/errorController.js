bf5115c4ecb8878390c1bb32747165d7f519ce0b
Blakcoder <anslemkay180@gmail.com>
2022-09-12 16:07:41 +0100

    Added New Error Handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,35 @@
+const sendErrorDev = (err, dev) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+const sendErrorProd = (err, res) => {
+  //OPerational Error that we trust, send to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming Errors or other unknown Error: Don't leak error details
+  } else {
+    console.error('ERROR 💣', err)
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+ 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
